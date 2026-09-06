efa0d43fc3248e4c8e1e749314fe9b1aa402b840
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-09-06 11:56:10 +0530

    Restore original website look and keep Cow Ghee Batti image

--- public/cow-ghee-image-fix.js
+++ public/cow-ghee-image-fix.js
@@ -1,5 +1,5 @@
 (function(){
-  var NEW_IMAGE='/products/cow ghee batti.jpg';
+  var NEW_IMAGE='https://user30197.na.imgto.link/public/20260906/190256.avif';
   function fix(){
     document.querySelectorAll('img').forEach(function(img){
       var src=img.getAttribute('src')||'';
